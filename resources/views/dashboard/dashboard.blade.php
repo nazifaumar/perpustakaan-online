0c5fd08a8cb02b2519cedfeab656ba631f26a3d1
nazifaumar <nazifaumar@smkwikrama.sch.id>
2023-01-17 19:55:09 +0700

    tampil users data

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -1,91 +1,126 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="style.css">
-    <link rel="shortcut icon" href="/assets/img/logo.png">
-    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@ttskch/select2-bootstrap4-theme@x.x.x/dist/select2-bootstrap4.min.css">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <link href="assets/img/book-bg.png" rel="icon">
-     <link rel="stylesheet" href="{{ asset('assets/css/dashboard.css') }}">
-     <link rel="stylesheet" href="{{ asset('assets/css/top.css') }}">
-     <title>Dashboard</title>
-   </head>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+	<!-- Boxicons -->
+	<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+	<!-- My CSS -->
+	<link rel="stylesheet" href="{{asset('assets/css/dash.css')}}">
+
+	<title>AdminHub</title>
+</head>
 <body>
-  <div class="sidebar">
-    <div class="logo-details">
-        <div class="logo_name">WIKBOOK</div>
-        <i class='bx bx-menu' id="btn" ></i>
-    </div>
-    <ul class="nav-list">
+
+
+	<!-- SIDEBAR -->
+	<section id="sidebar">
+		<a href="#" class="brand">
+			<i class='bx bxs-smile'></i>
+			<span class="text">AdminHub</span>
+		</a>
+		<ul class="side-menu top">
+			<li class="active">
+				<a href="#">
+					<i class='bx bxs-dashboard' ></i>
+					<span class="text">Dashboard</span>
+				</a>
+			</li>
       <li>
-        <a href="/create">
-          <i class='bx bx-grid-alt'></i>
-          <span class="links_name">Dashboard</span>
-        </a>
-         <span class="tooltip">Dashboard</span>
-      </li>
-      
+				<a href="#">
+					<i class='bx bxs-group' ></i>
+					<span class="text">Users</span>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<i class='bx bxs-shopping-bag-alt' ></i>
+					<span class="text">My Store</span>
+				</a>
+			</li>
+			
+			<li>
+				<a href="#">
+					<i class='bx bxs-message-dots' ></i>
+					<span class="text">Message</span>
+				</a>
+			</li>
       <li>
-       <a href="/user">
-         <i class='bx bx-user' ></i>
-         <span class="links_name">User</span>
-       </a>
-       <span class="tooltip">User</span>
-     </li>
+				<a href="/" class="logout">
+					<i class='bx bxs-log-out-circle' ></i>
+					<span class="text">Logout</span>
+				</a>
+			</li>
+	</section>
+	<!-- SIDEBAR -->
 
-     <li>
-      <a href="/">
-        <i class='bx bx-book-bookmark'></i>
-        <span class="links_name">Book</span>
-      </a>
-      <span class="tooltip">Book</span>
-    </li>
 
-    <li>
-      <a href="/">
-        <i class='bx bx-bookmark-plus'></i>
-        <span class="links_name">Category Book</span>
-      </a>
-       <span class="tooltip">Category Book</span>
-    </li>
-    <li>
-    
-    <li class="profile">
-      <div class="profile-details">
-        <div class="name"></div> 
-        <a href="/"><i class='bx bx-log-out' id="log_out"></i></a>
-      </div>
-    </li>
-    </ul>
-  </div>
-  
-      <script>
-      let sidebar = document.querySelector(".sidebar");
-      let closeBtn = document.querySelector("#btn");
-      let searchBtn = document.querySelector(".bx-search");
 
-      closeBtn.addEventListener("click", ()=>{
-        sidebar.classList.toggle("open");
-        menuBtnChange();
-      });
+	<!-- CONTENT -->
+	<section id="content">
+		<!-- NAVBAR -->
+		<nav>
+			<i class='bx bx-menu' ></i>
+		</nav>
+		<!-- NAVBAR -->
 
-      searchBtn.addEventListener("click", ()=>{ 
-        sidebar.classList.toggle("open");
-        menuBtnChange();
-      });
+		<!-- MAIN -->
+		<main>
+			<div class="head-title">
+				<div class="left">
+					<h1>Dashboard</h1>
+					<ul class="breadcrumb">
+						<li>
+							<a href="#">Dashboard</a>
+						</li>
+						<li><i class='bx bx-chevron-right' ></i></li>
+						<li>
+							<a class="active" href="#">Users</a>
+						</li>
+					</ul>
+				</div>
 
-      function menuBtnChange() {
-      if(sidebar.classList.contains("open")){
-        closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
-      }else {
-        closeBtn.classList.replace("bx-menu-alt-right","bx-menu");
-      }
-      }
-      </script>
+			<div class="table-data">
+				<div class="order">
+					<div class="head">
+						<h3>Users Data</h3>
+						<i class='bx bx-search' ></i>
+						<i class='bx bx-filter' ></i>
+					</div>
+					<table>
+						<thead>
+							<tr>
+								<th>No</th>
+								<th>Name</th>
+								<th>Email</th>
+                <th>Address</th>
+                <th>Number Phone</th>
+							</tr>
+						</thead>
+            <?php $i =1; ?>
+            @foreach($user as $item)
+						<tbody>
+							<tr>
+                <th>{{$i++}}</th>
+                <td class="data-title">{{$item->name}} </td>
+                <td class="data-title">{{$item->email}}</td>
+                <td class="data-title">{{$item->address}} </td>
+                <td class="data-title">{{$item->tlp}} </td>
+								<td>01-10-2021</td>
+								<td><span class="status completed">Delete</span></td>
+                @endforeach 
+							</tr>
+						</tbody>
+					</table>
+				</div>
+		</main>
+		<!-- MAIN -->
+	</section>
+	<!-- CONTENT -->
+	
 
+	<script src="assets/js/script.js"></script>
 </body>
-    @yield('content')
+
 </html>
